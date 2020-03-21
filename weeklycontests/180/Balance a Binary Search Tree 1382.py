0729bbc69ef6a03465d1caa828fec6b10e4ab9eb
ttungl <ttungl@gmail.com>
2020-03-20 18:09:17 -0700

    update 1382

--- weeklycontests/180/Balance a Binary Search Tree 1382.py
+++ weeklycontests/180/Balance a Binary Search Tree 1382.py
@@ -1,45 +1,45 @@
-1382. Balance a Binary Search Tree
+# 1382. Balance a Binary Search Tree
 
-Given a binary search tree, return a balanced binary search tree with the same node values.
+# Given a binary search tree, return a balanced binary search tree with the same node values.
 
-A binary search tree is balanced if and only if the depth of the two subtrees of every node never differ by more than 1.
+# A binary search tree is balanced if and only if the depth of the two subtrees of every node never differ by more than 1.
 
-If there is more than one answer, return any of them.
+# If there is more than one answer, return any of them.
 
-```
-1
- \
-  2 
-   \
-    3
-     \
-      4
-```
+# ```
+# 1
+#  \
+#   2 
+#    \
+#     3
+#      \
+#       4
+# ```
 
-```
-      2
-     / \
-    1   3
-    	 \
-    	  4
-```
-or
-```
-       3
-      / \
-     1   4
-      \
-       2
-```
+# ```
+#       2
+#      / \
+#     1   3
+#     	 \
+#     	  4
+# ```
+# or
+# ```
+#        3
+#       / \
+#      1   4
+#       \
+#        2
+# ```
 
-Input: root = [1,null,2,null,3,null,4,null,null]
-Output: [2,1,3,null,null,null,4]
-Explanation: This is not the only correct answer, [3,1,4,null,2,null,null] is also correct.
+# Input: root = [1,null,2,null,3,null,4,null,null]
+# Output: [2,1,3,null,null,null,4]
+# Explanation: This is not the only correct answer, [3,1,4,null,2,null,null] is also correct.
 
-Constraints:
+# Constraints:
 
-The number of nodes in the tree is between 1 and 10^4.
-The tree nodes will have distinct values between 1 and 10^5.
+# The number of nodes in the tree is between 1 and 10^4.
+# The tree nodes will have distinct values between 1 and 10^5.
 
 
 # Definition for a binary tree node.
